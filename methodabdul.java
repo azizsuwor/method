f66c81c63d32c7e97133d1fe76c416bfdf44b917
azizsuwor <163238328+azizsuwor@users.noreply.github.com>
2024-03-28 08:31:03 -0800

    Add files via upload

---
+++ methodabdul.java
@@ -0,0 +1,14 @@
+public class methodabdul {
+
+    public double luas_lingkaran(int diameter){
+        int jari2 = diameter / 10;
+        double luas = Math.PI * Math.pow(jari2,2);
+        return luas;
+        
+    }
+    public static void main(String[] args) {
+        method mt = new method ();
+        System.out.print(mt.luas_lingkaran(20));
+        System.exit(0);
+    }
+}
